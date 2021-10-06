8f7b9d8047b2a7cffec02a4c3aaeb938c02e9396
Misha Vashkevich <mihavi956@mail.ru>
2021-10-06 21:21:09 +0300

    custom table cell

---
+++ myPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  myPlaces
+//
+//  Created by Миша Вашкевич on 06.10.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+
+}
